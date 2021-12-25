1c7d3618f1144d36082540c68bba8bfa604742c1
unknown <vickygogeta1@gmail.com>
2021-12-26 00:27:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Blogr landing page
+
+## In Progress...
---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const menuBtn = document.querySelector('.menubtn');
+const navMenu = document.querySelector('.navlinkswrapper');
+const navLinks = document.querySelectorAll('.navlinkitem > a');
+
+const accordian = (elementArray, closestClass) => {
+  elementArray.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      const clickedLink = e.target.closest(closestClass);
+      clickedLink.classList.contains('open')
+        ? clickedLink.classList.remove('open')
+        : (resetAccordian(), clickedLink.classList.add('open'));
+    });
+  });
+
+  const resetAccordian = () => {
+    navLinks.forEach((link) => {
+      const allNavLinks = link.closest(closestClass);
+      if (allNavLinks.classList.contains('open'))
+        allNavLinks.classList.remove('open');
+    });
+  };
+};
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  navMenu.classList.toggle('open');
+});
+
+accordian(navLinks, '.navlinkitem');
